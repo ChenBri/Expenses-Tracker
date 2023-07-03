59117f23be1a2dc8972952d3ff517c7559487e7d
ChenBr <chenbrilling1@gmail.com>
2023-07-04 00:28:29 +0300

    Added expense adder prompt

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -22,6 +22,8 @@ const NewExpense = (props) => {
 
     if (!enteredTitle || !enteredPrice || !enteredDate) return;
 
+    boxHanlder();
+
     const expenseValues = {
       title: enteredTitle,
       price: enteredPrice,
@@ -36,43 +38,60 @@ const NewExpense = (props) => {
     setEnteredDate("");
   };
 
+  let [addBox, setAddBox] = useState(true);
+
+  function boxHanlder() {
+    setAddBox(!addBox);
+    setEnteredTitle("");
+    setEnteredPrice("");
+    setEnteredDate("");
+  }
+
   return (
     <div className="new-expense">
-      <form onSubmit={submitHandler}>
-        <div className="new-expense__controls">
-          <div className="new-expense__control">
-            <label>Title</label>
-            <input
-              type="text"
-              value={enteredTitle}
-              onChange={titleChangeHandler}
-            ></input>
-          </div>
-          <div className="new-expense__control">
-            <label>Price</label>
-            <input
-              type="number"
-              value={enteredPrice}
-              onChange={priceChangeHandler}
-              min="0.01"
-              step="0.01"
-            ></input>
-          </div>
-          <div className="new-expense__control">
-            <label>Date</label>
-            <input
-              type="date"
-              value={enteredDate}
-              onChange={dateChangeHandler}
-              min="2019-01-01"
-              max="2024-01-01"
-            ></input>
-          </div>
-        </div>
+      {addBox && (
         <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+          <button onClick={boxHanlder}>Add Expense</button>
         </div>
-      </form>
+      )}
+      {!addBox && (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              ></input>
+            </div>
+            <div className="new-expense__control">
+              <label>Price</label>
+              <input
+                type="number"
+                value={enteredPrice}
+                onChange={priceChangeHandler}
+                min="0.01"
+                step="0.01"
+              ></input>
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+                min="2019-01-01"
+                max="2024-01-01"
+              ></input>
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button onClick={boxHanlder}>Cancel</button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
